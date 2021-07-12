aa855cc1dfd11f24ae0cfab402090e543983b642
Igor Shevchenko <bnzone@gmail.com>
2021-07-11 20:05:30 -0700

    add heap

--- Trees/binary-search-tree.js
+++ Trees/binary-search-tree.js
@@ -55,5 +55,76 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  BreadthFirstSearch() {
+    let node = this.root;
+    const data = [];
+    const queue = [];
+
+    queue.push(this.root);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DepthFirstPreOrder() {
+    const data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
+  }
+
+  DepthFirstPostOrder() {
+    const data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(current);
+    return data;
+  }
+
+  DepthFirstInOrder() {
+    const data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
+  }
 }
 const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(20);
+tree.insert(3);
+tree.insert(8);
+tree.insert(9);
+tree.insert(2);
+tree.insert(1);
+
+console.log(tree.DepthFirstInOrder());
+console.log(tree.DepthFirstPreOrder());
+console.log(tree.DepthFirstPostOrder());
